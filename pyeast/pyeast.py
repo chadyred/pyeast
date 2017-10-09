0b10b074b130d7f35616526bf690001abac069bb
chadyred <chadyred@gmail.com>
2017-10-09 09:13:19 +0200

    Many add for better east.

--- pyeast/pyeast.py
+++ pyeast/pyeast.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import abc
-import time
-from typing import Callable
+import time, json, sys
+from typing import Callable, IO, cast
 
 class StartableClient(metaclass=abc.ABCMeta):
 
@@ -13,7 +13,7 @@ class StartableClient(metaclass=abc.ABCMeta):
 class StartableClientEnginify(StartableClient):
 
     @abc.abstractmethod
-    def if_warmup_do(self, url: str, action: Callable[[StartableClient], None]) -> StartableClient:
+    def if_warmup_do(self, url: str, action: Callable[['StartableClient'], None]) -> 'StartableClient':
         """check the warmup and path it througth action to handle behavior for recipient"""
 
 class EngineMakerInterface(metaclass=abc.ABCMeta):
@@ -35,9 +35,27 @@ class MessageFactoring(metaclass=abc.ABCMeta):
     def message(self, message) -> str:
         """Message factoring"""
 
+class UrlNormilizer(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def if_normalize_do(self, element: str, action: Callable([str], None)) -> 'UrlNormilizer':
+        """Normalize url entry"""
+
+class SimpleFormatter(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def format(self, element) -> 'SimpleFormatter':
+        """Message factoring"""
+
 class Browserify(metaclass=abc.ABCMeta):
+
     def naviguate(self):
         """Naviguate"""
+
+class Printer(metaclass=abc.ABCMeta):
+
+    def print_on(self, stream: IO[str]) -> 'Printer':
+        """Print on stream"""
 
 class StarterClient(StartableClient):
 
@@ -80,6 +98,48 @@ class SimpleStarterclient(StartableClient):
     def start(self, url, version) -> str:
         print("Version is " + version)
 
+class UrlNormilize(UrlNormilizer):
+
+    def __init__(self, clientEngine: StarterClientEngine):
+
+        self.url = clientEngine.url
+        self.url_format = clientEngine.url
+
+    def if_normalize_do(self, action: Callable([UrlNormilizer], None)) -> UrlNormilizer:
+
+        self.url_format =  "http://" + str(self.url)
+        action(self)
+
+        return self
+
+class Print(Printer):
+
+    def __init__(self, formatter: SimpleFormatter):
+
+        self.formatter = formatter
+
+    def print_on(self, stream: IO[str]) -> UrlNormilizer:
+
+        stream(self.formatter.content)
+
+        return self
+
+class SimpleFormatter(StartableClient):
+
+    def __init__(self, url_normilizer : UrlNormilizer):
+        self.url_normilizer = url_normilizer
+        self.content = ''
+
+    def to_json(self, action: Callable([SimpleFormatter], None)) -> str:
+
+        print("Json here !")
+
+        self.content = json.dumps({'url': self.url_normilizer.url_format}) + '\n'
+
+        action(self)
+
+        return self
+
 class StarterClientEngine(StartableClientEnginify):
 
     def __init__(self, engineMaker: EngineMakerInterface):
@@ -87,7 +147,10 @@ class StarterClientEngine(StartableClientEnginify):
 
     def if_warmup_do(self, url: str, action: Callable[[StartableClientEnginify], None]) -> StartableClientEnginify:
 
+        self.url = url
+
         action(self)
+
         return self
 
     def start(self, url, version) -> str:
@@ -114,7 +177,20 @@ class Naviguate():
     def browse(self, url):
         """Little call """
 
-        StarterClientEngine(BrowserNiviguate("Internet Exlorer") ).if_warmup_do(url, lambda browser: )
+        StarterClientEngine(
+            BrowserNiviguate("Internet Explorer").if_warmup_do(
+                url,
+                lambda clientEngine: UrlNormilizer(clientEngine).if_normalize_do(
+
+                    lambda urlNormilizer: SimpleFormatter(urlNormilizer).to_json(
+
+                        lambda formatter : Printer(formatter).print_on(
+                            sys.out
+                        )
+                    )
+                )
+            )
+        )
 
         return self
 
